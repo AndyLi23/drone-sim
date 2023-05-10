889aa624a18a68d03d0f18b62fd38e2fd2ac7b51
AndyLi23 <qingyuanli23@mittymonarch.com>
2023-05-09 20:09:30 -0700

    libgdx base project

---
+++ desktop/src/com/drone/sim/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.drone.sim;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.drone.sim.Sim;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("drone-sim");
+		new Lwjgl3Application(new Sim(), config);
+	}
+}
